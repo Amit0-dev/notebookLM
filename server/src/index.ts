acef09bc88b234a5cc8bf5723e234f5cc4f98d29
Amit0-dev <140780768+Amit0-dev@users.noreply.github.com>
2026-08-04 19:57:02 +0530

    feat: implement authentication service using Better Auth

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,17 @@
+import { toNodeHandler } from "better-auth/node";
 import "dotenv/config";
 import express from "express"
+import { auth } from "./lib/auth.js";
 
 
 const app = express()
-
 const PORT = process.env.PORT || 8080;
 
-app.get("/health", (req, res) => {
+app.all('/api/auth/{*any}', toNodeHandler(auth));
+app.use(express.json())
+
+
+app.get("/health", (_req, res) => {
     res.json({ message: "OK", timestamp: new Date() })
 })
 
